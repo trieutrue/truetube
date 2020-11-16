b20317e9c7ef8670c2a6a9b09b2f9aaf1b5c4763
JT <ttran2193@gmail.com>
2020-11-16 17:41:43 -0500

    Working on front end auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,24 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
 
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json ["There was an error with your Username/Password combination. Please try again."], status: 401
+    end
   end
 
   def destroy
-
+    if logged_in?
+      logout!
+      render json: { message: 'Logout successful' }
+    else
+      render json: {}, status: 404
+    end
   end
 end
