9e3810a282d52b8a2a0584eca487930ad12fb5db
JT <ttran2193@gmail.com>
2020-11-16 14:29:49 -0500

    create react entry file

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const root = document.getElementById('root');
-//   const store;
-//   ReactDOM.render()
-// })
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to React!</h1>, root)
+})
