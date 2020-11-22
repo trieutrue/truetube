d28fe7a6f2449a5d3dd70de3a58434e7754d299e
JT <ttran2193@gmail.com>
2020-11-22 16:03:42 -0500

    Filled content for side nav

--- frontend/components/nav/side_nav/side_nav.jsx
+++ frontend/components/nav/side_nav/side_nav.jsx
@@ -6,6 +6,7 @@ import { TiHome } from 'react-icons/ti'
 import { ImFire } from 'react-icons/im'
 import { RiVideoLine } from 'react-icons/ri'
 import { AiFillLike } from 'react-icons/ai'
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
 import * as MD from 'react-icons/md'
 
 
@@ -15,12 +16,13 @@ class SideNav extends React.Component {
     const { currentUser } = this.props
     return currentUser ? (
       <>
-        <RiVideoLine />
-        <AiFillLike />
+        <li><RiVideoLine /></li>
+        <li><AiFillLike /></li>
       </>
     ) : (
       <>
         <li>
+          <p>Sign in to like videos, comment, and subscribe.</p>
           <Link to="/signin" className="signin-btn">
             <FontAwesomeIcon icon={faUserCircle} className="user-icon" />
             <strong>SIGN IN</strong>
@@ -37,19 +39,48 @@ class SideNav extends React.Component {
     return (
       <nav id="side-nav">
         <header className="side-header">
-          <TiHome />
-          <ImFire />
-          <MD.MdSubscriptions />
+          <ul>
+            <li><TiHome /></li>
+            <li><ImFire /></li>
+            <li><MD.MdSubscriptions /></li>
+          </ul>
         </header>
 
-        <header className="side-body">
-          <MD.MdVideoLibrary />
-          <MD.MdHistory />
-          {this.subHeader()}
+        <header className="side-sub-header">
+          <ul>
+            <li><MD.MdVideoLibrary /></li>
+            <li><MD.MdHistory /></li>
+            {this.subHeader()}
+          </ul>
         </header>
 
         <footer>
+          <ul className="link-footer">
+            <li><FaGithubSquare /></li>
+            <li><FaLinkedin /></li>
+          </ul>
 
+          <ul className="info-foot"> 
+            <li>
+              <ul>
+                <li>About </li>
+                <li>Press</li>
+                <li>Copyright</li>
+                <li>Contact us</li>
+                <li>Creators</li>
+                <li>Advertise</li>
+                <li>Developers</li>
+              </ul>
+              <ul>
+                <li>Terms</li>
+                <li>Privacy</li>
+                <li>Policy & Safety</li>
+                <li>How WeTube works</li>
+                <li>Test new features</li>
+              </ul>
+            </li>
+            <li>© 2020 WeTube</li>
+          </ul>
         </footer>
       </nav>
     )
