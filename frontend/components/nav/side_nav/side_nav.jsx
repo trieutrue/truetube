5aa998368560fcf8f985482a73059f29c22001f0
JT <ttran2193@gmail.com>
2020-11-22 19:43:44 -0500

    Finish styling side nav bar

--- frontend/components/nav/side_nav/side_nav.jsx
+++ frontend/components/nav/side_nav/side_nav.jsx
@@ -21,7 +21,7 @@ class SideNav extends React.Component {
       </>
     ) : (
       <>
-        <li>
+        <li className="signin-li no-hover" >
           <p>Sign in to like videos, comment, and subscribe.</p>
           <Link to="/signin" className="signin-btn">
             <FontAwesomeIcon icon={faUserCircle} className="user-icon" />
@@ -40,47 +40,37 @@ class SideNav extends React.Component {
       <nav id="side-nav">
         <header className="side-header">
           <ul>
-            <li><TiHome /></li>
-            <li><ImFire /></li>
-            <li><MD.MdSubscriptions /></li>
+            <li><TiHome />Home</li>
+            <li><ImFire />Trending</li>
+            <li><MD.MdSubscriptions />Subscriptions</li>
           </ul>
-        </header>
 
-        <header className="side-sub-header">
           <ul>
-            <li><MD.MdVideoLibrary /></li>
-            <li><MD.MdHistory /></li>
+            <li><MD.MdVideoLibrary />Library</li>
+            <li><MD.MdHistory />History</li>
             {this.subHeader()}
           </ul>
         </header>
 
         <footer>
           <ul className="link-footer">
-            <li><FaGithubSquare /></li>
-            <li><FaLinkedin /></li>
+            <a href="https://github.com/ttran193/wetube" target="_blank">
+              <li onClick="location.href = 'https://github.com/ttran193/wetube';">
+                <FaGithubSquare />Github
+              </li>
+            </a>
+            <a href="https://www.linkedin.com/in/trieu-tran-52339a167/" target="_blank">
+              <li onClick="location.href = 'https://www.linkedin.com/in/trieu-tran-52339a167/';">
+                <FaLinkedin />LinkedIn
+              </li>
+            </a>
           </ul>
 
-          <ul className="info-foot"> 
-            <li>
-              <ul>
-                <li>About </li>
-                <li>Press</li>
-                <li>Copyright</li>
-                <li>Contact us</li>
-                <li>Creators</li>
-                <li>Advertise</li>
-                <li>Developers</li>
-              </ul>
-              <ul>
-                <li>Terms</li>
-                <li>Privacy</li>
-                <li>Policy & Safety</li>
-                <li>How WeTube works</li>
-                <li>Test new features</li>
-              </ul>
-            </li>
-            <li>© 2020 WeTube</li>
+          <ul className="info-footer no-hover"> 
+            <li>About Press Copyright <br />Contact us Creators <br />Advertise Developers</li>
+            <li>Terms Privacy Policy & Safety <br />How WeTube works <br />Test new features</li>
           </ul>
+          <h6>© 2020 WeTube</h6>
         </footer>
       </nav>
     )
