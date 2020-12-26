bda553853b7632a67b12c81c260f7012f96d1c3c
JT <ttran2193@gmail.com>
2020-12-26 13:54:42 -0700

    Change video nav functional component to class component

--- frontend/components/nav/video_nav/video_nav.jsx
+++ frontend/components/nav/video_nav/video_nav.jsx
@@ -4,50 +4,54 @@ import { formatVideoShowDate } from '../../../util/date_util'
 import * as MD from 'react-icons/md'
 import { IoMdShareAlt } from 'react-icons/io'
 
-const VideoNav = ({ video, user }) => {
-  return (
-    <>
-      <div className="video-nav">
-        <h1>{video.title}</h1> 
-        <div className="row nav-bar">
-            <div className="left-row">
-              <p>123,285 views</p>
-              <i className="dot">•</i>
-              <p>{formatVideoShowDate(video.createdAt)}</p>
-            </div>
+class VideoNav extends React.Component {
+  render() {
+    const { video, user } = this.props
 
-            <div className="right-row">
-              <ul>
-                <div className="likes">
-                  <li><MD.MdThumbUp />{video.upvotes}</li>
-                  <li><MD.MdThumbDown />{video.downvotes}</li>
-                </div>
-                <li><IoMdShareAlt />SHARE</li>
-                <li><MD.MdPlaylistAdd />SAVE</li>
-                <li><MD.MdMoreHoriz /></li>
-              </ul>
-            </div>
-        </div>
-      </div>
+    return (
+      <>
+        <div className="video-nav">
+          <h1>{video.title}</h1> 
+          <div className="row nav-bar">
+              <div className="left-row">
+                <p>123,285 views</p>
+                <i className="dot">•</i>
+                <p>{formatVideoShowDate(video.createdAt)}</p>
+              </div>
 
-      <div className="info-box">
-        <div className="row">
-          <div className="left-row">
-            <Link to={`/channel/${user.id}/featured`}><div className="profile-icon">{user.channelName[0]}</div></Link>
-            <div className="column">
-              <Link to={`/channel/${user.id}/featured`}><h6>{user.channelName}</h6></Link>
-              <p>660K subscribers</p>
-            </div>
+              <div className="right-row">
+                <ul>
+                  <div className="likes">
+                    <li><MD.MdThumbUp />{video.upvotes}</li>
+                    <li><MD.MdThumbDown />{video.downvotes}</li>
+                  </div>
+                  <li><IoMdShareAlt />SHARE</li>
+                  <li><MD.MdPlaylistAdd />SAVE</li>
+                  <li><MD.MdMoreHoriz /></li>
+                </ul>
+              </div>
           </div>
-
-          <button className="subscribe-btn">SUBSCRIBE</button>
         </div>
-        <p className="description">{video.description}</p>
-      </div>
+
+        <div className="info-box">
+          <div className="row">
+            <div className="left-row">
+              <Link to={`/channel/${user.id}/featured`}><div className="profile-icon">{user.channelName[0]}</div></Link>
+              <div className="column">
+                <Link to={`/channel/${user.id}/featured`}><h6>{user.channelName}</h6></Link>
+                <p>660K subscribers</p>
+              </div>
+            </div>
+
+            <button className="subscribe-btn">SUBSCRIBE</button>
+          </div>
+          <p className="description">{video.description}</p>
+        </div>
 
 
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default VideoNav;
