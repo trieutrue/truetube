80a7b36ae6a7d5d1c5a5027ffd26dabb361da807
JT <ttran2193@gmail.com>
2021-01-07 13:42:31 -0700

    Add votes to reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import videosReducer from './videos_reducer';
 import commentsReducer from './comments_reducer';
+import votesReducer from './votes_reducer';
 
 export default combineReducers({
   users: usersReducer,
   videos: videosReducer,
-  comments: commentsReducer
+  comments: commentsReducer,
+  votes: votesReducer
 })
