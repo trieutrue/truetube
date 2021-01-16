81e37b4b33edf750817edc8abc047598f90094df
JT <ttran2193@gmail.com>
2021-01-16 00:29:06 -0700

    Refactor video index and index item for search queries

--- frontend/components/videos/video_index_container.js
+++ frontend/components/videos/video_index_container.js
@@ -2,12 +2,14 @@ import { connect } from 'react-redux';
 import VideoIndex from './video_index';
 import { fetchVideos, deleteVideo } from '../../actions/video_actions';
 import { openModal } from '../../actions/modal_actions';
+import { fetchSearchQuery } from '../../actions/filter_actions';
 
 const mSTP = state => {
   return {
-    videos: Object.values(state.entities.videos),
+    videos: state.entities.videos,
     users: state.entities.users,
-    currentUser: state.entities.users[state.session.id]
+    currentUser: state.entities.users[state.session.id],
+    filters: state.ui.filters
   }
 }
 
@@ -15,7 +17,8 @@ const mDTP = dispatch => {
   return {
     fetchVideos: () => dispatch(fetchVideos()),
     openModal: modal => dispatch(openModal(modal)),
-    deleteVideo: videoId => dispatch(deleteVideo(videoId))
+    deleteVideo: videoId => dispatch(deleteVideo(videoId)),
+    fetchSearchQuery: searchQuery => dispatch(fetchSearchQuery(searchQuery))
   }
 }
 
