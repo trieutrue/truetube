33878155eebaa94d0a3268b0562f28d42e89eaeb
JT <ttran2193@gmail.com>
2021-01-28 10:24:51 -0700

    Debug empty filters

--- frontend/reducers/filters_reducer.js
+++ frontend/reducers/filters_reducer.js
@@ -3,7 +3,7 @@ import { UPDATE_FILTER } from '../actions/filter_actions';
 const filtersReducer = (state = [], action) => {
   Object.freeze(state);
   if (action.type === UPDATE_FILTER) {
-    return Object.keys(action.videos).map(n => parseInt(n))
+    return action.videos ? Object.keys(action.videos).map(n => parseInt(n)) : []
   } else {
     return state
   }
